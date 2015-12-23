f6ee58354846ab428224807d9672c026f74e82af
Hagen <spam@xgerdax.de>
2015-12-23 17:24:04 +0100

    Added .obj loading

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
-const gulp = require('gulp');
-const babel = require('gulp-babel');
-const eslint = require('gulp-eslint');
+const gulp = require('gulp'),
+    babel = require('gulp-babel'),
+    connect = require('gulp-connect'),
+    eslint = require('gulp-eslint'),
+    notif = require('gulp-notify'),
+    watch = require('gulp-watch');
 
 // Lint that script!
 gulp.task('lint', function() {
@@ -13,22 +16,37 @@ gulp.task('lint', function() {
 // Copy dependencies to dist/js
 gulp.task('deps', function() {
     return gulp.src([
-        'node_modules/three/three.js'    
+        'node_modules/three/three.js'
     ]).pipe(gulp.dest('dist/js'));
 });
 
 // Copy html and css
 gulp.task('assets', ['deps'], function () {
     return gulp.src([
-        'src/index.html'
+        'src/**/*', '!src/js'
     ]).pipe(gulp.dest('dist/'));
 });
 
 // after linting and copying we're doing the babel thing
-gulp.task('default', ['lint', 'assets'], function() {
+gulp.task('babel', ['lint', 'assets'], function() {
     return gulp.src('src/js/**/*.js')
         .pipe(babel({
             presets: ['es2015']
         }))
         .pipe(gulp.dest('dist/js'));
 });
+
+gulp.task('connect', function() {
+  connect.server({
+    'root': 'dist'
+  });
+});
+
+gulp.task('watch:js', ['babel'], function() {
+    watch('src/js/**/*.js', { emitOnGlob: true }, function() {
+        gulp.run('babel');
+    });
+});
+
+gulp.task('default', ['babel']);
+gulp.task('serve', ['babel', 'watch:js', 'connect']);
--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,39 @@
-// Expression bodies
-var odds = evens.map(v => v + 1);
-var nums = evens.map((v, i) => v + i);
+var scene, camera, renderer, loader, obj;
 
-// Statement bodies
-nums.forEach(v => {
-  if (v % 5 === 0)
-    fives.push(v);
-});
+function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 5;
 
-// Lexical this
-var bob = {
-  _name: "Bob",
-  _friends: [],
-  printFriends() {
-    this._friends.forEach(f =>
-      console.log(this._name + " knows " + f));
-  }
-};
+        // add spot light
+    var spLight = new THREE.SpotLight(0xffffff, 1.75, 2000, Math.PI / 3);
+    spLight.castShadow = true;
+    spLight.position.set(-100, 300, -50);
+    scene.add(spLight);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    loader = new THREE.OBJLoader();
+    loader.load('obj/1.obj', function (object) {
+            object.traverse(function (child) {
+                if (child instanceof THREE.Mesh) {
+                    child.material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+                }
+            });
+            obj = object;
+            scene.add(obj);
+            render();
+        }
+    );
+}
+
+function render() {
+    requestAnimationFrame(render);
+    obj.rotation.x += 0.01;
+    obj.rotation.y = Math.sin(performance.now() * 0.001) * 4 + 4;
+    renderer.render(scene, camera);
+}
+
+init();
